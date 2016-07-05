7dd9d4e900732808622d06cd09228461a31905cb
Karl Hepler <khepler@brandmovers.com>
2016-07-05 10:28:40 -0400

    enable early return before if statement in jshint

--- libs/defaults.js
+++ libs/defaults.js
@@ -4,7 +4,8 @@ module.exports = {
     jshint: {
         esversion: 6,
         laxbreak: true,
-        "-W086": true // Allow switch/case fall-through
+        "-W086": true, // Allow switch/case fall-through
+        "-W027": true  // Allow early return before if statements
     },
     plumber: {},
     php: {
